e2c0d3d592e9c32b60729ae09cdc3ed0a393e562
Colin Zeidler <c.zeidler.wc@gmail.com>
2023-03-05 22:05:34 -0500

    Add project files.

---
+++ KSP2 Modded Toggle/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KSP2_Modded_Toggle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ModManager modM;
+        public MainWindow()
+        {
+            InitializeComponent();
+            modM = new ModManager();
+
+            kspDir.Text = modM.kspPath;
+            modsDir.Text = modM.modsPath;
+
+            SetModState();
+        }
+
+        private void KspDirChange(object sender, RoutedEventArgs e)
+        {
+            modM.kspPath = kspDir.Text;
+        }
+
+        private void ModsDirChange(object sender, RoutedEventArgs e)
+        {
+            modM.modsPath = modsDir.Text;
+        }
+
+        private void SetModState()
+        {
+            if (modM.ModsEnabled)
+            {
+                modState.Content = "Modded";
+                modsEnabled.IsChecked = true;
+            }
+            else
+            {
+                modState.Content = "Un-Modded";
+            }
+        }
+
+        private void ButtonApplyClick(object sender, RoutedEventArgs e)
+        {
+            if (modsEnabled.IsChecked == true)
+            {
+                Console.WriteLine("Enabling Mods");
+                modM.enableMods();
+            } else
+            {
+                Console.WriteLine("Disabling Mods");
+                modM.disableMods();
+            }
+
+            SetModState();
+        }
+    }
+}
---
+++ KSP2 Modded Toggle/ModManager.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.IO;
+
+namespace KSP2_Modded_Toggle
+{
+
+    internal class ModManager
+    {
+        public string kspPath;
+        public string modsPath;
+        public bool ModsEnabled { get; private set; }
+
+
+        public readonly string[] items =
+        {
+            $"BepInEx{Path.DirectorySeparatorChar}",
+            "doorstop_config.ini",
+            "winhttp.dll"
+        };
+
+        public ModManager()
+        {
+            kspPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Kerbal Space Program 2";
+            modsPath = "";
+
+            checkMods();
+        }
+
+        private void checkMods()
+        {
+            ModsEnabled = Directory.Exists(composePath(kspPath, items[0])) && File.Exists(composePath(kspPath, items[1]));
+        }
+
+        private static string composePath(string dir, string item)
+        {
+            return Path.Join(dir, item);
+        }
+
+        public void enableMods()
+        {
+            foreach (var item in this.items)
+            {
+                try
+                {
+                    if (Path.EndsInDirectorySeparator(item))
+                    {
+                        Directory.CreateSymbolicLink(composePath(this.kspPath, item), composePath(modsPath, item));
+                    }
+                    else
+                    {
+                        File.CreateSymbolicLink(composePath(this.kspPath, item), composePath(modsPath, item));
+                    }
+                } catch (IOException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            this.ModsEnabled = true;
+        }
+
+        public void disableMods()
+        {
+            foreach(var item in this.items)
+            {
+                try
+                {
+                    if (Path.EndsInDirectorySeparator(item))
+                    {
+                        Directory.Delete(composePath(kspPath, item));
+                    }
+                    else
+                    {
+                        File.Delete(composePath(this.kspPath, item));
+                    }
+                } catch (IOException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            this.ModsEnabled = false;
+        }
+    }
+}
